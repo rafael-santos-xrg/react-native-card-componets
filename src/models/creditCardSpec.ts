ff7010cc43ade484a37728a2e2edd441213e8f08
victor-santos-xrg <victorsantos.pba@outlook.com>
2024-09-06 15:03:40 -0300

    ADD: None return

--- src/models/creditCardSpec.ts
+++ src/models/creditCardSpec.ts
@@ -1,25 +1,29 @@
 import creditCardType from "credit-card-type";
 
-export const getCreditCardType = (creditCardNumber: string) => {
+interface getCreditCardTypeReturn {
+  type: string;
+  gaps: number[];
+  lengths: number[];
+  code: {
+    size: number;
+    name: string;
+  };
+}
+
+export const getCreditCardType = (creditCardNumber: string): getCreditCardTypeReturn => {
   const cardInformationMatches = creditCardType(creditCardNumber);
   if (cardInformationMatches.length === 0) {
-    return;
+    return {
+      type: "none",
+      gaps: [4, 8, 12],
+      lengths: [16],
+      code: {
+        size: 3,
+        name: "CVC",
+      },
+    };
   }
   const { type, gaps, lengths, code } = cardInformationMatches[0];
   const cardInformationFirstMatch = { type, gaps, lengths, code };
-
-  console.log(cardInformationFirstMatch);
+  return cardInformationFirstMatch;
 };
-
-export function insertSpaces(targetString: string, gapsList: number[]) {
-  let result = targetString;
-  gapsList.forEach((gap, index) => {
-    const position = gap + index;
-    if (position < result.length) {
-      result = result.slice(0, position) + " " + result.slice(position);
-    }
-  });
-  return result;
-}
-
-const ex = { code: { name: "CVV", size: 3 }, gaps: [4, 8, 12], lengths: [16, 18, 19], type: "visa" };
